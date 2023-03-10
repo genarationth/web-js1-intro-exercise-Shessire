5a7471afd004143375df994e54d30295185501a7
Shessire <125847387+Shessire@users.noreply.github.com>
2023-03-10 04:55:34 +0000

    add prompt

---
+++ script.js
@@ -0,0 +1,11 @@
+function ChangeName(){
+    let text;
+    let person = prompt("What is your name?");
+    if (person == null || person == "") {
+      text = "User cancelled the prompt.";
+    } else {
+      text = "Hello " + person;
+    }
+    document.getElementById("inputName").innerHTML = text;
+  
+}
